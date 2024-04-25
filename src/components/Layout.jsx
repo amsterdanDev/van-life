a5b713c00f2f09d7e67e2ab733afef4196ed065b
Amsterdan Vasconcelos <165407326+amsterdanDev@users.noreply.github.com>
2024-04-25 20:13:01 -0300

    feet: create footer

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,6 @@
-import Header from './Header'
 import { Outlet } from 'react-router-dom'
+import Footer from './Footer'
+import Header from './Header'
 
 const Layout = () => {
   return (
@@ -8,6 +9,7 @@ const Layout = () => {
       <main>
         <Outlet />
       </main>
+      <Footer />
     </>
   )
 }

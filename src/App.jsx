dbc496929461f55d754e9c1071050219dff2e076
Amsterdan Vasconcelos <165407326+amsterdanDev@users.noreply.github.com>
2024-04-25 10:35:06 -0300

    Feat: bootstrapped Home and About pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom"
+import About from "./pages/About"
+import Home from "./pages/Home"
+import './index.css'
+
 function App() {
 
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+      <header>
+        <Link className="site-logo" to="/">#VanLife</Link>
+        <nav>
+          <Link to="/about">About</Link>
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
